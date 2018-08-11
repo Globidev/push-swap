3f08d4db322345ff2b86e37e8230d069aae043f0
Guillaume Depardon <guillaume.depardon@gmail.com>
2018-08-11 19:21:00 +0200

    Optimized Astar

--- src/solver/astar.rs
+++ src/solver/astar.rs
@@ -4,17 +4,12 @@ use std::collections::{VecDeque, HashSet, hash_map::DefaultHasher};
 use std::hash::{Hash, Hasher};
 
 pub struct Astar {
-    moves: Vec<Instruction>
+    moves: VecDeque<Instruction>
 }
 
 impl Astar {
     pub fn new(stack: impl Stack<N>) -> Self {
-        let moves = solve(stack)
-            .into_iter()
-            .rev()
-            .collect();
-
-        Self { moves }
+        Self { moves: solve(stack) }
     }
 }
 
@@ -22,21 +17,18 @@ impl Iterator for Astar {
     type Item = Instruction;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.moves.pop()
+        self.moves.pop_front()
     }
 }
 
-fn solve(stack: impl Stack<N>) -> Vec<Instruction> {
+fn solve(stack: impl Stack<N>) -> VecDeque<Instruction> {
     let mut open_set = VecDeque::new();
     let mut closed_set = HashSet::new();
 
-    open_set.push_back(Node {
-        a: stack,
-        ..Default::default()
-    });
+    open_set.push_back(Node { a: stack, ..Default::default() });
 
     while let Some(node) = open_set.pop_front() {
-        if node.a.is_sorted() && node.b.len() == 0 {
+        if node.b.len() == 0 && node.a.is_sorted() {
             return node.instrs
         }
 
@@ -55,7 +47,7 @@ fn solve(stack: impl Stack<N>) -> Vec<Instruction> {
 struct Node<S> {
     pub a: S,
     pub b: S,
-    instrs: Vec<Instruction>
+    instrs: VecDeque<Instruction>
 }
 
 impl<S: Stack<N>> PartialEq for Node<S> {
@@ -81,127 +73,68 @@ fn hash<T: Hash>(t: &T) -> u64 {
 
 fn neighbors<S: Stack<N>>(node: Node<S>) -> impl Iterator<Item = Node<S>> {
     use self::Instruction::*;
+    type ValidateInstruction = fn(usize, usize, &Instruction) -> bool;
 
-    const INSTRS: [Instruction; 11] = [
-        ReverseRotateBoth, ReverseRotateA, ReverseRotateB,
-        RotateBoth, RotateA, RotateB,
-        SwapBoth, SwapA, SwapB,
-        PushA, PushB,
+    const INSTRS: [(Instruction, ValidateInstruction); 11] = [
+        (RotateBoth, |a_len, b_len, instr|
+            a_len >= 2 && b_len >= 2
+                && !instr_among(instr, &[RRotateA, RRotateB, RRotateBoth, RotateA, RotateB])
+        ),
+        (RotateA, |a_len, _, instr|
+            a_len >= 2 && !instr_among(instr, &[RRotateA, RRotateB, RRotateBoth])
+        ),
+        (RotateB, |_, b_len, instr|
+            b_len >= 2 && !instr_among(instr, &[RRotateA, RRotateB, RRotateBoth])
+        ),
+        (RRotateBoth, |a_len, b_len, instr|
+            a_len >= 2 && b_len >= 2
+                && !instr_among(instr, &[RotateA, RotateB, RotateBoth, RRotateA, RRotateB])
+        ),
+        (RRotateA, |a_len, _, instr|
+            a_len >= 2 && !instr_among(instr, &[RotateA, RotateB, RotateBoth])
+        ),
+        (RRotateB, |_, b_len, instr|
+            b_len >= 2 && !instr_among(instr, &[RotateA, RotateB, RotateBoth])
+        ),
+        (SwapBoth, |a_len, b_len, instr|
+            a_len >= 2 && b_len >= 2 && !instr_among(instr, &[SwapA, SwapB, SwapBoth])
+        ),
+        (SwapA, |a_len, _, instr|
+            a_len >= 2 && !instr_among(instr, &[SwapA, SwapB, SwapBoth])
+        ),
+        (SwapB, |_, b_len, instr|
+            b_len >= 2 && !instr_among(instr, &[SwapA, SwapB, SwapBoth])
+        ),
+        (PushA, |_, b_len, instr|
+            b_len > 0 && instr != &PushB
+        ),
+        (PushB, |a_len, _, instr|
+            a_len >= 2 && instr != &PushA
+        ),
     ];
 
-    INSTRS.iter()
-        .filter_map(move |i|
-            match valid_instr(i, &node) {
-                true  => Some(transform_instr(i, &node)),
-                false => None
-            }
-        )
-}
-
-fn valid_instr<S: Stack<N>>(instr: &Instruction, node: &Node<S>) -> bool {
-    use self::Instruction::*;
-
-    let last_instr = node.instrs.last().unwrap_or(&PushB);
     let a_len = node.a.len();
     let b_len = node.b.len();
+    let last_instr = node.instrs.back().cloned().unwrap_or(PushB);
 
-    match instr {
-        PushA => {
-            let b_non_empty = b_len > 0;
-            let last_not_push_b = last_instr != &PushB;
-            b_non_empty && last_not_push_b
-        },
-        PushB => {
-            let a_at_least_2 = a_len >= 2;
-            let last_not_push_a = last_instr != &PushA;
-            a_at_least_2 && last_not_push_a
-        },
+    INSTRS.iter()
+        .filter(move |(_, valid_instr)| valid_instr(a_len, b_len, &last_instr))
+        .map(move |(instr, _)| transform_instr(instr, &node))
+}
 
-        SwapA => {
-            let last_not_swap =
-                last_instr != &SwapA &&
-                last_instr != &SwapB &&
-                last_instr != &SwapBoth;
-            let at_least_2 = a_len >= 2;
-            last_not_swap && at_least_2
-        },
-        SwapB => {
-            let last_not_swap =
-                last_instr != &SwapA &&
-                last_instr != &SwapB &&
-                last_instr != &SwapBoth;
-            let at_least_2 = b_len >= 2;
-            last_not_swap && at_least_2
-        },
-        SwapBoth => {
-            let last_not_swap =
-                last_instr != &SwapA &&
-                last_instr != &SwapB &&
-                last_instr != &SwapBoth;
-            let at_least_2 = a_len >= 2 && b_len >= 2;
-            last_not_swap && at_least_2
-        },
-
-        RotateA => {
-            let last_not_rotate =
-                last_instr != &ReverseRotateA &&
-                last_instr != &ReverseRotateB &&
-                last_instr != &ReverseRotateBoth;
-            let at_least_2 = a_len >= 2;
-            last_not_rotate && at_least_2
-        },
-        RotateB => {
-            let last_not_rotate =
-                last_instr != &ReverseRotateA &&
-                last_instr != &ReverseRotateB &&
-                last_instr != &ReverseRotateBoth;
-            let at_least_2 = b_len >= 2;
-            last_not_rotate && at_least_2
-        },
-        RotateBoth => {
-            let last_not_rotate =
-                last_instr != &ReverseRotateA &&
-                last_instr != &ReverseRotateB &&
-                last_instr != &ReverseRotateBoth &&
-                last_instr != &RotateA &&
-                last_instr != &RotateB;
-            let at_least_2 = a_len >= 2 && b_len >= 2;
-            last_not_rotate && at_least_2
-        },
-
-        ReverseRotateA => {
-            let last_not_rotate =
-                last_instr != &RotateA &&
-                last_instr != &RotateB &&
-                last_instr != &RotateBoth;
-            let at_least_2 = a_len >= 2;
-            last_not_rotate && at_least_2
-        },
-        ReverseRotateB => {
-            let last_not_rotate =
-                last_instr != &RotateA &&
-                last_instr != &RotateB &&
-                last_instr != &RotateBoth;
-            let at_least_2 = b_len >= 2;
-            last_not_rotate && at_least_2
-        },
-        ReverseRotateBoth => {
-            let last_not_rotate =
-                last_instr != &RotateA &&
-                last_instr != &RotateB &&
-                last_instr != &RotateBoth &&
-                last_instr != &ReverseRotateA &&
-                last_instr != &ReverseRotateB;
-            let at_least_2 = a_len >= 2 && b_len >= 2;
-            last_not_rotate && at_least_2
-        },
+fn instr_among(instr: &Instruction, set: &[Instruction]) -> bool {
+    for set_instr in set {
+        if instr == set_instr {
+            return true
+        }
     }
+    false
 }
 
 fn transform_instr<S: Stack<N>>(instr: &Instruction, n: &Node<S>) -> Node<S> {
     let mut node = n.clone();
 
-    node.instrs.push(instr.clone());
+    node.instrs.push_back(instr.clone());
 
     execute(instr, &mut node.a, &mut node.b);
 
